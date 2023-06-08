f7111bd99b1c9eb33c18ba2930c289d401307b2c
baris <bariscan.ersoylu@gmail.com>
2023-06-08 22:47:02 +0300

    added sounds

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-// Audio sources for each button
 const audioSources = {
-    "75db": "sounds/140dB.mp3",
-    "80db": "sounds/140dB.mp3",
-    "88db": "sounds/140dB.mp3",
-    "90db": "sounds/140dB.mp3",
-    "100db": "sounds/140dB.mp3",
-    "105db": "sounds/140dB.mp3",
-    "110db": "sounds/140dB.mp3",
-    "120db": "sounds/140dB.mp3",
-    "130db": "sounds/140dB.mp3",
-    "140db": "sounds/140dB.mp3"
+    "75db": "sounds/washing-machine.wav",
+    "80db": "sounds/mower.wav",
+    "88db": "sounds/city.wav",
+    "90db": "sounds/blender.wav",
+    "100db": "sounds/honk.wav",
+    "105db": "sounds/music.wav",
+    "110db": "sounds/bark.wav",
+    "120db": "sounds/siren.wav",
+    "130db": "sounds/jet.wav",
+    "140db": "sounds/fireworks.mp3"
 };
 
 // Map to store the state of each button
 const buttonStates = {};
 
-// Add click event listener to each button
+// Add click, mouseover, and mouseout event listeners to each button
 const buttons = document.getElementsByClassName("button");
 for (let i = 0; i < buttons.length; i++) {
     const button = buttons[i];
     buttonStates[button.id] = {
         isPlaying: false,
         audioElement: new Audio(audioSources[button.id]),
-        imgElement: button.getElementsByTagName("img")[0], // Get the img element within the button
+        imgElement: button.getElementsByTagName("img")[0] // Get the img element within the button
     };
-    button.addEventListener("click", toggleSound);
+    button.addEventListener("click", handleClick);
+    button.addEventListener("mouseover", handleMouseOver);
+    button.addEventListener("mouseout", handleMouseOut);
 }
 
-function toggleSound() {
+function handleClick() {
     const button = this;
     const buttonState = buttonStates[button.id];
 
@@ -41,13 +42,48 @@ function toggleSound() {
         // Play sound
         buttonState.audioElement.play();
 
-        // Change src attribute of the image to include "-play"
+        // Change src attribute of the image to include "-play" only if not already present
         const imgSrc = buttonState.imgElement.src;
-        const imgSrcPlay = imgSrc.replace(".svg", "-play.svg");
-        buttonState.imgElement.src = imgSrcPlay;
+        if (!imgSrc.includes("-play")) {
+            const imgSrcPlay = imgSrc.replace(".svg", "-play.svg");
+            buttonState.imgElement.src = imgSrcPlay;
+        }
 
         // Update button state to true
         buttonState.isPlaying = true;
+
+        // Listen for the "ended" event to remove "-play" when sound finishes
+        buttonState.audioElement.addEventListener("ended", function () {
+            buttonState.imgElement.src = buttonState.imgElement.src.replace(
+                "-play.svg",
+                ".svg"
+            );
+            buttonState.isPlaying = false;
+        });
+    }
+}
+
+function handleMouseOver() {
+    const button = this;
+    button.classList.add("hovered");
+
+    const buttonState = buttonStates[button.id];
+    const imgSrc = buttonState.imgElement.src;
+    if (!imgSrc.includes("-play")) {
+        const imgSrcPlay = imgSrc.replace(".svg", "-play.svg");
+        buttonState.imgElement.src = imgSrcPlay;
+    }
+}
+
+function handleMouseOut() {
+    const button = this;
+    button.classList.remove("hovered");
+
+    const buttonState = buttonStates[button.id];
+    const imgSrc = buttonState.imgElement.src;
+    if (imgSrc.includes("-play") && !buttonState.isPlaying) {
+        const imgSrcNormal = imgSrc.replace("-play.svg", ".svg");
+        buttonState.imgElement.src = imgSrcNormal;
     }
 }
 
